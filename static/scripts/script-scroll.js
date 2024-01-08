95e8eec51d4be4d2f7eae57e2eb8778265ef93a4
Luis <luiscorbachoflores@gmail.com>
2024-01-08 21:54:20 +0100

    puede que script definitivo #3

--- static/scripts/script-scroll.js
+++ static/scripts/script-scroll.js
@@ -2,10 +2,11 @@
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
 var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos ||  window.scrollY < 50) {
-    document.getElementById("NavBar").style.top = "0";
+console.log(currentScrollPos);
+  if (prevScrollpos > currentScrollPos ||  window.scrollY < (window.innerHeight)*0.08) {
+    document.getElementById("topnav").style.top = "0";
   } else {
-    document.getElementById("NavBar").style.top = "-50px";
+    document.getElementById("topnav").style.top = "-8vh";
   }
   prevScrollpos = currentScrollPos;
 }
