a6c80941d52eee723061be6f6e6be4a2f5b94ebd
Luis <luiscorbachoflores@gmail.com>
2024-01-08 21:00:08 +0100

    bounce safari

--- static/scripts/script-scroll.js
+++ static/scripts/script-scroll.js
@@ -1,11 +1,11 @@
 /* en el móvil, para esconder la barra de navegación al hacer scroll hacia abajo */
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
-  var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("topnav").style.top = "0";
+var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos ||  window.pageYOffset < 50) {
+    document.getElementById("NavBar").style.top = "0";
   } else {
-    document.getElementById("topnav").style.top = "-8vh";
+    document.getElementById("NavBar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
-} 
+}
